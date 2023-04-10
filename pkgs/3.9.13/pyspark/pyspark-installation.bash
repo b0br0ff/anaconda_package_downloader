73331c551d3ddd1040ca9998d26dceab76fc48fc
b0br0ff <ebobuh@gmail.com>
2023-04-10 18:48:30 +0200

    Create pyspark-installation.bash

---
+++ pkgs/3.9.13/pyspark/pyspark-installation.bash
@@ -0,0 +1,46 @@
+#https://conda.anaconda.org/conda-forge/linux-64/abseil-cpp-20210324.2-h9c3ff4c_0.tar.bz2
+conda install abseil-cpp-20210324.2-h9c3ff4c_0tar.bz2
+#https://conda.anaconda.org/conda-forge/linux-64/aws-c-common-0.6.2-h7f98852_0.tar.bz2
+conda install aws-c-common-0.6.2-h7f98852_0tar.bz2
+#https://conda.anaconda.org/conda-forge/linux-64/gflags-2.2.2-he1b5a44_1004.tar.bz2
+conda install gflags-2.2.2-he1b5a44_1004tar.bz2
+#https://conda.anaconda.org/conda-forge/linux-64/libutf8proc-2.7.0-h7f98852_0.tar.bz2
+conda install libutf8proc-2.7.0-h7f98852_0tar.bz2
+#https://conda.anaconda.org/conda-forge/linux-64/re2-2021.09.01-h9c3ff4c_0.tar.bz2
+conda install re2-2021.09.01-h9c3ff4c_0tar.bz2
+#https://conda.anaconda.org/conda-forge/linux-64/aws-c-cal-0.5.11-h95a6274_0.tar.bz2
+conda install aws-c-cal-0.5.11-h95a6274_0tar.bz2
+#https://conda.anaconda.org/conda-forge/linux-64/aws-checksums-0.1.11-ha31a3da_7.tar.bz2
+conda install aws-checksums-0.1.11-ha31a3da_7tar.bz2
+#https://conda.anaconda.org/conda-forge/linux-64/glog-0.5.0-h48cff8f_0.tar.bz2
+conda install glog-0.5.0-h48cff8f_0tar.bz2
+#https://conda.anaconda.org/conda-forge/linux-64/libprotobuf-3.18.0-h780b84a_1.tar.bz2
+conda install libprotobuf-3.18.0-h780b84a_1tar.bz2
+#https://conda.anaconda.org/conda-forge/linux-64/s2n-1.0.10-h9b69904_0.tar.bz2
+conda install s2n-1.0.10-h9b69904_0tar.bz2
+#https://conda.anaconda.org/conda-forge/linux-64/aws-c-io-0.10.5-hfb6a706_0.tar.bz2
+conda install aws-c-io-0.10.5-hfb6a706_0tar.bz2
+#https://conda.anaconda.org/conda-forge/linux-64/grpc-cpp-1.40.0-h05f19cf_1.tar.bz2
+conda install grpc-cpp-1.40.0-h05f19cf_1tar.bz2
+#https://repo.anaconda.com/pkgs/main/linux-64/libthrift-0.15.0-hcc01f38_0.tar.bz2
+conda install libthrift-0.15.0-hcc01f38_0tar.bz2
+#https://conda.anaconda.org/conda-forge/linux-64/orc-1.7.0-h68e2c4e_0.tar.bz2
+conda install orc-1.7.0-h68e2c4e_0tar.bz2
+#https://conda.anaconda.org/conda-forge/linux-64/aws-c-event-stream-0.2.7-h3541f99_13.tar.bz2
+conda install aws-c-event-stream-0.2.7-h3541f99_13tar.bz2
+#https://conda.anaconda.org/conda-forge/linux-64/aws-sdk-cpp-1.8.186-hb4091e7_3.tar.bz2
+conda install aws-sdk-cpp-1.8.186-hb4091e7_3tar.bz2
+#https://conda.anaconda.org/conda-forge/noarch/py4j-0.10.9-pyh9f0ad1d_0.tar.bz2
+conda install py4j-0.10.9-pyh9f0ad1d_0tar.bz2
+#https://conda.anaconda.org/conda-forge/linux-64/python_abi-3.9-2_cp39.tar.bz2
+conda install python_abi-3.9-2_cp39tar.bz2
+#https://conda.anaconda.org/conda-forge/linux-64/conda-22.9.0-py39hf3d152e_2.tar.bz2
+conda install conda-22.9.0-py39hf3d152e_2tar.bz2
+#https://conda.anaconda.org/conda-forge/linux-64/arrow-cpp-5.0.0-py39h247a7eb_8_cpu.tar.bz2
+conda install arrow-cpp-5.0.0-py39h247a7eb_8_cputar.bz2
+#https://conda.anaconda.org/conda-forge/noarch/parquet-cpp-1.5.1-2.tar.bz2
+conda install parquet-cpp-1.5.1-2tar.bz2
+#https://conda.anaconda.org/conda-forge/linux-64/pyarrow-5.0.0-py39h3ebc44c_8_cpu.tar.bz2
+conda install pyarrow-5.0.0-py39h3ebc44c_8_cputar.bz2
+#https://conda.anaconda.org/conda-forge/noarch/pyspark-3.1.1-pyh44b312d_0.tar.bz2
+conda install pyspark-3.1.1-pyh44b312d_0tar.bz2
